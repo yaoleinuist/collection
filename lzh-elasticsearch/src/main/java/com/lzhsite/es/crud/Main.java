523a49408177e2dd13a46139a9b184c69b608e7f
6231969224@qq.com <6231969224@qq.com>
2017-05-02 15:29:06 +0800

    加入elasticsearch测试代码

---
+++ lzh-elasticsearch/src/main/java/com/lzhsite/es/crud/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.lzhsite.es.crud;
+
+/**
+ * @author xialei(xialei199023@163.com)
+ * @version v1.0 2015-10-7ÉÏÎç11:21:03
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		EsCrud crud = new EsCrud(ClientFactory.transportClient());
+		String index = "person";
+		String type = "test";
+		crud.createIndex(index);
+		//crud.addMapping(index, type);
+		//crud.deleteIndex(index);
+		
+		//crud.createDoc(index, type, "2");
+		//crud.updateDoc(index, type, "2");
+		//System.out.println(crud.get(index, type, "1"));
+		//System.out.println(crud.queryByFilter(index, type));
+		//crud.deleteByQuery(index, type);
+		//crud.min(index, type);
+	}
+}
