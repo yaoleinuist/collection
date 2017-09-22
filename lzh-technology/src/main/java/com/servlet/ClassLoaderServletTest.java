0a5e15ec8ea6b6bbb19f2b1dfa72ffb8cca00c04
lzhcoder <6231969242@qq.com>
2017-09-22 15:57:51 +0800

    深入理解calssload

---
+++ lzh-technology/src/main/java/com/servlet/ClassLoaderServletTest.java
@@ -0,0 +1,49 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ClassLoaderServletTest
+ */
+public class ClassLoaderServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClassLoaderServletTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+  
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+        ClassLoader loader = this.getClass().getClassLoader();  
+        while(loader != null) {  
+            out.write(loader.getClass().getName()+"<br/>");  
+            loader = loader.getParent();  
+        }  
+        out.write(String.valueOf(loader));  
+        out.flush();  
+        out.close();  
+    }  
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ lzh-technology/src/main/java/com/technology/jvm/classLoader/NetClassLoaderSimple.java
@@ -0,0 +1,10 @@
+package com.technology.jvm.classLoader;
+
+public class NetClassLoaderSimple {
+	
+	private NetClassLoaderSimple instance;
+
+	public void setNetClassLoaderSimple(Object obj) {
+		this.instance = (NetClassLoaderSimple) obj;
+	}
+}
