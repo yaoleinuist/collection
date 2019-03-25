2b6535adbdd7822c2f7bb1f89624c3844fd1ee6b
lzhcoder <6231969242@qq.com>
2019-03-25 09:53:09 +0800

    双重校验锁实现对象单例（线程安全）

--- lzh-technology/src/main/java/com/lzhsite/technology/designPattern/singleton/DoubleCheckedLock.java
+++ lzh-technology/src/main/java/com/lzhsite/technology/designPattern/singleton/DoubleCheckedLock.java
@@ -2,14 +2,27 @@ package com.lzhsite.technology.designPattern.singleton;
 
  
 
-/*不可否认，synchronized关键字是可以保证单例，但是程序的性能却不容乐观，
-原因在于getInstance()整个方法体都是同步的，这就限定了访问速度。
-其实我们需要的仅仅是在首次初始化对象的时候需要同步，
-对于之后的获取不需要同步锁。因此，可以做进一步的改进：*/
+/**
+ * 不可否认，synchronized关键字是可以保证单例，但是程序的性能却不容乐观，
+ * 原因在于getInstance()整个方法体都是同步的，这就限定了访问速度。
+ * 其实我们需要的仅仅是在首次初始化对象的时候需要同步，
+ * 对于之后的获取不需要同步锁。因此，可以做进一步的改进：
+ */
 
 public class DoubleCheckedLock {  
 	
-    private static DoubleCheckedLock instance;    
+	/**
+	 * 另外，需要注意 instance 采用 volatile 关键字修饰也是很有必要。
+     * instance 采用 volatile 关键字修饰也是很有必要的， instance = new DoubleCheckedLock(); 这段代码其实是分为三步执行：
+     * 为 instance 分配内存空间
+     * 初始化 instance
+     * 将 instance 指向分配的内存地址
+     * 但是由于 JVM 具有指令重排的特性，执行顺序有可能变成 1->3->2。指令重排在单线程环境下不会出先问题，
+     * 但是在多线程环境下会导致一个线程获得还没有初始化的实例。例如，线程 T1 执行了 1 和 3，此时 T2 
+     * 调用 getUniqueInstance() 后发现 uniqueInstance 不为空，因此返回 instance，但此时 instance 还未被初始化。
+     * 使用 volatile 可以禁止 JVM 的指令重排，保证在多线程环境下也能正常运行。
+	 */
+    private volatile  static DoubleCheckedLock instance;    
         
     public static DoubleCheckedLock getInstance() {    
         if (instance == null) {  //step1  
